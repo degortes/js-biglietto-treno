7e1d09f2d4b5b94fcad26e23288de0468233d0e1
Danilo <danilodegortes@gmail.com>
2020-10-13 16:41:59 +0200

    Set tofixed to see 2 decimals

--- main.js
+++ main.js
@@ -9,14 +9,11 @@ document.getElementById('distance').innerHTML = distance;
 if ( isNaN(age) || isNaN(distance) ) {
     alert('Devi inserire dei numeri!!! aggiorna la pagina e riprova')
 } else if (age >= 18 && age <= 65) {
-    console.log('il biglietto costa ' + Math.round(price*100)/100);
-    document.getElementById('price').innerHTML = Math.round(price*100)/100;
+    document.getElementById('price').innerHTML = price.toFixed(2);
 } else if (age < 18) {
-    console.log('il biglietto costa ' + Math.round((price-(price*0.2))*100)/100 + ' sei giovane e hai lo sconto del 20%');
-    document.getElementById('price').innerHTML = Math.round((price-(price*0.2))*100)/100;
+    document.getElementById('price').innerHTML = (price-(price*0.2)).toFixed(2);
     document.getElementById('fare').innerHTML = 'tariffa Junior';
 } else {
-    console.log('il biglietto costa ' + Math.round((price-(price*0.4))*100)/100 + ' sei vecchio e hai lo sconto del 40%');
-    document.getElementById('price').innerHTML = Math.round((price-(price*0.4))*100)/100;
+    document.getElementById('price').innerHTML = (price-(price*0.4)).toFixed(2);
     document.getElementById('fare').innerHTML = 'tariffa Senior';
 }
