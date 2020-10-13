1ed8715a03837ddcb45dde1610c167984c5770bf
Danilo <danilodegortes@gmail.com>
2020-10-13 16:07:52 +0200

    aggiunto alert tolte variabili e reindentato

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-var age = prompt('inserisci la tua età');
-var distance = prompt('inserisci i Km da percorrere');
+var age = parseInt(prompt('inserisci la tua età'));
+var distance = parseInt(prompt('inserisci i Km da percorrere'));
 var price = distance*0.21;
-var senior_fare = price-(price*0.2)
-var junior_fare = price-(price*0.4)
 
 
 document.getElementById('distance').innerHTML = distance;
 
 
-if (age < 18) {
-    console.log('il biglietto costa ' + Math.round(junior_fare*100)/100 + ' sei giovane e hai lo sconto del 20%');
-    document.getElementById('price').innerHTML = Math.round(junior_fare*100)/100;
-    document.getElementById('fare').innerHTML = 'tariffa Junior';
-} else if (age > 65) {
-    console.log('il biglietto costa ' + Math.round(senior_fare*100)/100 + ' sei vecchio e hai lo sconto del 40%');
-    document.getElementById('price').innerHTML = Math.round(senior_fare*100)/100;
-    document.getElementById('fare').innerHTML = 'tariffa Senior';
-
-
-} else {
+if ( isNaN(age) || isNaN(distance) ) {
+    alert('Devi inserire dei numeri!!! aggiorna la pagina e riprova')
+} else if (age >= 18 && age <= 65) {
     console.log('il biglietto costa ' + Math.round(price*100)/100);
     document.getElementById('price').innerHTML = Math.round(price*100)/100;
-
+} else if (age < 18) {
+    console.log('il biglietto costa ' + Math.round((price-(price*0.2))*100)/100 + ' sei giovane e hai lo sconto del 20%');
+    document.getElementById('price').innerHTML = Math.round((price-(price*0.2))*100)/100;
+    document.getElementById('fare').innerHTML = 'tariffa Junior';
+} else {
+    console.log('il biglietto costa ' + Math.round((price-(price*0.4))*100)/100 + ' sei vecchio e hai lo sconto del 40%');
+    document.getElementById('price').innerHTML = Math.round((price-(price*0.4))*100)/100;
+    document.getElementById('fare').innerHTML = 'tariffa Senior';
 }
